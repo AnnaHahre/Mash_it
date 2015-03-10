bb8950e1e3fc078e6b7fe7e65ed193934a300dd6
Elle <michelle.pantorp@bredband.net>
2015-03-10 08:15:09 +0100

    More comments

--- static/script.js
+++ static/script.js
@@ -1,364 +1,366 @@
-      $(document).ready(function(){
-        $('#submit_color').bind("click", getPalette); //COLORS
+$(document).ready(function(){
+  $('#submit_color').bind("click", getPalette); //COLORS
 
-        $("#fonta").bind("click", getFonts); //FONT
+  $("#fonta").bind("click", getFonts); //FONT
 
-        window.onbeforeunload = function() {
-          return "Are you sure you want to leave? Your changes will not be saved!";
-        }
+  window.onbeforeunload = function() {
+    return "Are you sure you want to leave? Your changes will not be saved!";
+  }
 
-        //*------------------ LOADING OF TEMPLATES
-        //*
-        $('#template_one').bind('click', function(){
-          $('#template').load('views/template_one.html'); 
+  //*------------- LOADING OF TEMPLATES --------------
+  //*
+  $('#template_one').bind('click', function(){
+    $('#template').load('views/template_one.html'); 
 
+  });
+
+  $('#template_two').bind('click', function(){
+    $('#template').load('views/template_two.html');
+
+  });
+
+
+  //*---------------- TAB-CONTROLL ----------------
+  //*
+  //Set the initial state: highlight the first button...
+  $('#tabcontrols').find('li:eq(0)').addClass('selected');
+ 
+  //and hide all slides except the first one
+  $('#tabs').find('> div:eq(0)').nextAll().hide();
+ 
+  //actions that apply on click of any of the buttons
+  $('#tabcontrols li').click( function(event) {
+ 
+    //turn off the link so it doesn't try to jump down the page
+    event.preventDefault();
+ 
+    //un-highlight the buttons
+    $('#tabcontrols li').removeClass();
+ 
+    //hide all the slides
+    $('#tabs > div').hide();
+ 
+    //highlight the current button
+    $(this).addClass('selected');
+ 
+    //get the index of the current button...
+    var index = $('#tabcontrols li').index(this);
+ 
+    //and use that index to show the corresponding slide
+    $('#tabs > div:eq('+index+')').show();
+ 
+  });
+
+  //*------------- CODE FOR SLIDING DASHBOARD ----------------
+  //*
+  var operator = '+=';
+  $('#dashboard').click(function(e){
+    e.stopPropagation();
+    $('#dashboard').animate({left:operator + '-300'}, 1000);
+      if(operator == '+='){
+        operator = '-=';
+      }
+      else{
+        operator = '+=';
+      }
+  });
+
+  $('input, #tabcontrols, #category').click(function(event){
+    event.stopPropagation();
+  });
+
+});
+
+//*----------------- COLOR-SCRIPT ------------------
+//*
+function getPalette(){
+  $('#palette').empty();
+  var hex = $('#colors').val();
+  event.preventDefault();
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:1234/palette/" +hex,
+    dataType: "json",
+
+    success: function(response) {
+     showPalette(response);
+    },
+    error: function() {
+      alert('Not working!');
+      }
+    });
+  $('#colors').val("");
+}
+
+function showPalette(response){
+  $.each(response, function(index, value) {
+    $.each(value, function(index, color) {
+      //if (color.length == 5){
+        $.each(color, function(index, item) {
+           $('#palette').append("<div class='col' value='#" + item + "'><p class='col_p' style=background-color:#" +item + ";' value=" +item+ "></p><p class='hex_name'>#" +item+"</p></div>");
         });
+     // }
+    });
+  });
+   $('.col').bind("click", changeStyle);
+}
 
-        $('#template_two').bind('click', function(){
-          $('#template').load('views/template_two.html');
+//*---------------- SCRIPT FOR CHANGING STYLES ON WEBPAGE ----------------
+//*
+function changeStyle(e){
+  //delete choice
+  choice = $(this).attr('value');
 
-        });
-
-
-          //*---------------- TAB-CONTROLL
-          //*
-          //Set the initial state: highlight the first button...
-          $('#tabcontrols').find('li:eq(0)').addClass('selected');
-         
-          //and hide all slides except the first one
-          $('#tabs').find('> div:eq(0)').nextAll().hide();
-         
-          //actions that apply on click of any of the buttons
-          $('#tabcontrols li').click( function(event) {
-         
-            //turn off the link so it doesn't try to jump down the page
-            event.preventDefault();
-         
-            //un-highlight the buttons
-            $('#tabcontrols li').removeClass();
-         
-            //hide all the slides
-            $('#tabs > div').hide();
-         
-            //highlight the current button
-            $(this).addClass('selected');
-         
-            //get the index of the current button...
-            var index = $('#tabcontrols li').index(this);
-         
-            //and use that index to show the corresponding slide
-            $('#tabs > div:eq('+index+')').show();
-         
-          });
-
-        var operator = '+=';
-        $('#dashboard').click(function(e){
-          e.stopPropagation();
-          $('#dashboard').animate({left:operator + '-300'}, 1000);
-            if(operator == '+='){
-              operator = '-=';
-            }
-            else{
-              operator = '+=';
-            }
-        });
-
-        $('input, #tabcontrols, #category').click(function(event){
-          event.stopPropagation();
-        });
-        
+  $('#template h1, h2, h3, h4, span, footer p, p').click(function(e){
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $(e.target).css({
+        'color' : choice
       });
+    }
+    else {
+      $(e.target).css({
+      'font-family' : choice
+       });
+    }
+  });
 
-      //*----------------- COLOR-SCRIPT ------------------
-      //*
-      function getPalette(){
-        $('#palette').empty();
-        var hex = $('#colors').val();
-        event.preventDefault();
-        $.ajax({
-          type: "GET",
-          url: "http://localhost:1234/palette/" +hex,
-          dataType: "json",
+  $('.list-unstyled a').click(function(e){
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.list-unstyled a').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.list-unstyled a').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-          success: function(response) {
-           showPalette(response);
-          },
-          error: function() {
-            alert('Not working!');
-            }
-          });
-        $('#colors').val("");
-      }
+  $('.featurette p').click(function(e){
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.featurette p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette p').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-      function showPalette(response){
-        $.each(response, function(index, value) {
-          $.each(value, function(index, color) {
-            //if (color.length == 5){
-              $.each(color, function(index, item) {
-                 $('#palette').append("<div class='col' value='#" + item + "'><p class='col_p' style=background-color:#" +item + ";' value=" +item+ "></p><p class='hex_name'>#" +item+"</p></div>");
-              });
-           // }
-          });
-        });
-         $('.col').bind("click", changeStyle);
-      }
+  $('nav a').click(function(e){
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('nav a').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('nav a').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-      //*---------------- SCRIPT FOR CHANGING STYLES ON WEBPAGE ----------------
-      //*
-      function changeStyle(e){
-        //delete choice
-        choice = $(this).attr('value');
+  $('header').click(function(e){
+    e.stopPropagation();
+    $('header').css({
+      'background-color' : choice
+    });
+  });
 
-        $('#template h1, h2, h3, h4, span, footer p, p').click(function(e){
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $(e.target).css({
-              'color' : choice
-            });
-          }
-          else {
-            $(e.target).css({
-            'font-family' : choice
-             });
-          }
-        });
+  $('nav').click(function(e){
+    e.stopPropagation();
+    $('nav').css({
+      'background-color' : choice
+    });
+  });
+/*
+  
+  $('header h1').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('header h1').css({
+      'color' : choice
+       });
 
-        $('.list-unstyled a').click(function(e){
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.list-unstyled a').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.list-unstyled a').css({
-              'font-family' : choice
-            });
-          }
-        });
+    }
+    else {
+      $('header h1').css({
+      'font-family' : choice
+       });
+    }
+  });
 
-        $('.featurette p').click(function(e){
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.featurette p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette p').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('header h2').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('header h2').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('header h2').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('nav a').click(function(e){
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('nav a').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('nav a').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('.featurette h1').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.featurette h1').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette h1').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('header').click(function(e){
-          e.stopPropagation();
-          $('header').css({
-            'background-color' : choice
-          });
-        });
+  $('.featurette h2').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.featurette h2').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette h2').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('nav').click(function(e){
-          e.stopPropagation();
-          $('nav').css({
-            'background-color' : choice
-          });
-        });
-     /*
-        
-        $('header h1').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('header h1').css({
-            'color' : choice
-             });
+  $('span').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('span').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('span').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-          }
-          else {
-            $('header h1').css({
-            'font-family' : choice
-             });
-          }
-        });
+  $('.featurette p').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.featurette p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette p').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('header h2').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('header h2').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('header h2').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('.lead p').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.lead p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette p').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('.featurette h1').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.featurette h1').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette h1').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('.media p').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.media p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette p').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('.featurette h2').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.featurette h2').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette h2').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('.well p').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('.well p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('.featurette p').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('span').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('span').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('span').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('nav a').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('nav a').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('nav a').css({
+        'font-family' : choice
+      });
+    }
+  });
 
-        $('.featurette p').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.featurette p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette p').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('nav').click(function (e) {
+    e.stopPropagation();
+    $('nav').css({
+      'background-color' : choice,
+      'border' : '1px solid' + choice
+    });
+  });
 
-        $('.lead p').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.lead p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette p').css({
-              'font-family' : choice
-            });
-          }
-        });
+  /*$('.featurette-image').click(function (e) {
+    e.stopPropagation();
+    $('.featurette-image').css({
+      'border' : '2px solid #' + hex
+    });
+  });
 
-        $('.media p').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.media p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette p').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('header').click(function (e) {
+    e.stopPropagation();
+    $('header').css({
+      'background-color' : choice
+    });
+  });
 
-        $('.well p').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('.well p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('.featurette p').css({
-              'font-family' : choice
-            });
-          }
-        });
+  $('footer p').click(function (e) {
+    e.stopPropagation();
+    if (choice.substring(0,1) == "#") {
+      $('footer p').css({
+        'color' : choice
+      });
+    }
+    else {
+      $('footer p').css({
+        'font-family' : choice
+      });
+    }
+  });*/
+}
 
-        $('nav a').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('nav a').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('nav a').css({
-              'font-family' : choice
-            });
-          }
-        });
 
-        $('nav').click(function (e) {
-          e.stopPropagation();
-          $('nav').css({
-            'background-color' : choice,
-            'border' : '1px solid' + choice
-          });
-        });
-
-        /*$('.featurette-image').click(function (e) {
-          e.stopPropagation();
-          $('.featurette-image').css({
-            'border' : '2px solid #' + hex
-          });
-        });
-
-        $('header').click(function (e) {
-          e.stopPropagation();
-          $('header').css({
-            'background-color' : choice
-          });
-        });
-
-        $('footer p').click(function (e) {
-          e.stopPropagation();
-          if (choice.substring(0,1) == "#") {
-            $('footer p').css({
-              'color' : choice
-            });
-          }
-          else {
-            $('footer p').css({
-              'font-family' : choice
-            });
-          }
-        });*/
-      }
-
-      
 
 
 
